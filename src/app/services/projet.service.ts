d60f4a977e8f79dcb8006a53611d6ff822576340
fpiuzzi <florent.piuzzi@edu.igensia.com>
2024-12-22 23:56:53 +0100

    Correction tri

---
+++ src/app/services/projet.service.ts
@@ -0,0 +1,30 @@
+// src/app/services/project.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private apiUrl = 'http://localhost:3000/projects';
+
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>(this.apiUrl);
+  }
+
+  addProject(project: Project): Observable<Project> {
+    return this.http.post<Project>(this.apiUrl, project);
+  }
+
+  updateProject(project: Project): Observable<Project> {
+    return this.http.put<Project>(`${this.apiUrl}/${project.id}`, project);
+  }
+
+  deleteProject(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
